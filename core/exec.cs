b951f78ab13bf07c06c81e8468d43a15f3480f16
ConanODoyle <samuel.w.sze@gmail.com>
2019-07-03 14:40:29 -0700

    Add execs, move core

---
+++ core/exec.cs
@@ -0,0 +1,8 @@
+echo("");
+echo("--Executing AI--");
+exec("./ai/basic.cs");
+exec("./ai/callbacks.cs");
+exec("./ai/search.cs");
+
+echo("");
+echo("--Executing weapons--");
---
+++ server.cs
@@ -0,0 +1,11 @@
+echo("");
+echo("----Executing libraries----");
+exec("./lib/Support_RPGDamage.cs");
+
+echo("");
+echo("----Executing utils----");
+exec("./util/getClosestObjectToPoint.cs");
+
+echo("");
+echo("----Executing core files----");
+exec("./core/exec.cs");
