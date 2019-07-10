ed50f8dbdcecd2250010b9452082c0730fa89d13
ConanODoyle <samuel.w.sze@gmail.com>
2019-07-10 00:51:44 -0700

    Add ranged goblin type

--- core/ai/mobs/goblin.cs
+++ core/ai/mobs/goblin.cs
@@ -1,7 +1,9 @@
 if (!isObject($RPGData_Goblin))
 {
 	$RPGData_Goblin = new ScriptObject(RPGDataObject) {};
+	$RPGData_GoblinArcher = new ScriptObject(RPGDataObject) {};
 	MissionCleanup.add($RPGData_Goblin);
+	MissionCleanup.add($RPGData_GoblinArcher);
 }
 
 if (!isObject($RPGData_GoblinAttack))
@@ -17,9 +19,9 @@ if (!isObject($RPGData_GoblinAttack))
 	$RPGData_GoblinRangedAttack = new ScriptObject(GoblinRangedAttackType) {
 		attackImage = "bowImage";
 		triggerTime = "1 1";
-		minRange = 6;
-		maxRange = 15;
-		stopRange = 9;
+		minRange = 0;
+		maxRange = 18;
+		stopRange = 1000;
 		backingAttack = 1;
 	};
 
@@ -46,7 +48,36 @@ $RPGData_Goblin.expReward = 10;
 $RPGData_Goblin.maxForwardSpeed = 4.5;
 $RPGData_Goblin.maxBackwardSpeed = 2.8;
 $RPGData_Goblin.maxSideSpeed = 2.8;
-$RPGData_Goblin.maxYawSpeed = 5;
+$RPGData_Goblin.maxYawSpeed = 10;
 $RPGData_Goblin.maxPitchSpeed = 10;
 $RPGData_Goblin.scale = "1 1 1";
 $RPGData_Goblin.botDB = "PlayerStandardArmor";
+
+
+
+
+
+
+$RPGData_GoblinArcher.name = "Goblin Archer";
+$RPGData_GoblinArcher.maxDamage = 80;
+$RPGData_GoblinArcher.armor = 0;
+$RPGData_GoblinArcher.resist = 0;
+$RPGData_GoblinArcher.level = -500;
+
+$RPGData_GoblinArcher.searchType = "NearRadiusLOSSearchType";
+
+$RPGData_GoblinArcher.attackType = "GoblinAttackType";
+
+$RPGData_GoblinArcher.botLogicFunc = "MRPG_simpleLogic";
+$RPGData_GoblinArcher.botActionFunc = "MRPG_simpleAction";
+$RPGData_GoblinArcher.damageCallbackFunc = "MRPG_simpleDamageCallback";
+
+$RPGData_GoblinArcher.expReward = 10;
+
+$RPGData_GoblinArcher.maxForwardSpeed = 0;
+$RPGData_GoblinArcher.maxBackwardSpeed = 0;
+$RPGData_GoblinArcher.maxSideSpeed = 0;
+$RPGData_GoblinArcher.maxYawSpeed = 5;
+$RPGData_GoblinArcher.maxPitchSpeed = 5;
+$RPGData_GoblinArcher.scale = "1 1 1";
+$RPGData_GoblinArcher.botDB = "PlayerStandardArmor";
