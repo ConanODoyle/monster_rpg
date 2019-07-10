8d27cbb0a792e41408188e002392ccbbc2d815da
ConanODoyle <samuel.w.sze@gmail.com>
2019-07-10 13:16:06 -0700

    Add automatic spawning code

--- core/ai/spawn.cs
+++ core/ai/spawn.cs
@@ -6,6 +6,11 @@ function MRPGBot_Spawn(%brick, %RPGData)
 		talk("ERROR (spawn.cs): AI Set nonexistent! Executing logic.cs...");
 		exec("./logic.cs");
 	}
+	else if (isObject(%brick.hBot))
+	{
+		%brick.hBot.delete();
+	}
+
 	%pos = vectorAdd(%brick.getPosition(), "0 0 " @ getWord(%RPGData.scale, 2) * 3 / 2);
 	%rot = getWords(%brick.getSpawnPoint(), 3, 6);
 	%spawn = %pos SPC %rot;
@@ -36,5 +41,73 @@ function MRPGBot_Spawn(%brick, %RPGData)
 	%bot.spawnExplosion("spawnProjectile", getWord(%RPGData.scale, 2));
 
 	$MRPG_AISet.add(%bot);
-	%bot.nextThink = ($Sim::Time + 2 | 0) | 0;
+	%bot.nextThink = ($Sim::Time + 3 | 0) | 0;
 }
+
+
+
+
+if (!isObject(MRPG_SpawnSet))
+{
+	$MRPG_SpawnSet = new SimSet(MRPG_SpawnSet) {};
+	MissionCleanup.add($MRPG_SpawnSet);
+}
+
+
+function spawnTick(%idx)
+{
+	cancel($masterSpawnSchedule);
+	if (!isObject(MRPG_SpawnSet))
+	{
+		return;
+	}
+
+	%count = MRPG_SpawnSet.getCount();
+
+	if (%idx >= %count)
+	{
+		%idx = 0;
+	}
+
+	for (%i = 0; %i < 16; %i++)
+	{
+		if (%idx >= %count)
+		{
+			break;
+		}
+		%curr = MRPG_SpawnSet.getObject(%idx);
+
+		%name = strReplace(getSubStr(%curr, 1, 100), "_", " ");
+		%dataType = getWord(%name, 0);
+		%time = getMax(1, getWord(%name, 1));
+		if (isObject(%data = $RPGData_[%dataType]))
+		{
+			if (isObject(%curr.hBot))
+			{
+				%curr.lastHadBot = $Sim::Time;
+			}
+			else if ($Sim::Time - %curr.lastHadBot > %time)
+			{
+				MRPGBot_Spawn(%curr, %data);
+				%curr.lastHadBot = $Sim::Time;
+			}
+		}
+
+		%idx++;
+	}
+
+	$masterSpawnSchedule = schedule(1, 0, logicTick, %idx);
+}
+
+package MRPG_SpawnPackage 
+{
+	function fxDTSBrickData::onAdd(%this, %obj)
+	{
+		if (%this.isMRPGSpawn && %obj.isPlanted)
+		{
+			MRPG_SpawnSet.add(%obj);
+		}
+		return parent::onAdd(%this, %obj);
+	}
+};
+activatePackage(MRPG_SpawnPackage);
