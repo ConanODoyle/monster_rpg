d980739b3b1c5a14d9119d5a4dfb04a5ee9b615b
ConanODoyle <samuel.w.sze@gmail.com>
2019-07-11 17:41:06 -0700

    Remove level records, add spawnPlayer check for lack of MRPG values

--- core/valueCheck.cs
+++ core/valueCheck.cs
@@ -13,5 +13,19 @@ package MRPG_ValueCheckPackage
 		}
 		return %ret;
 	}
+
+	function GameConnection::spawnPlayer(%cl)
+	{
+		%ret = parent::spawnPlayer(%cl);
+		if (%cl.maxDamage $= "")
+		{
+			%cl.maxDamage = 100;
+			%cl.armor = 0;
+			%cl.resist = 0;
+			%cl.level = 1;
+			%cl.class = "None";
+		}
+		return %ret;
+	}
 };
 schedule(100, 0, activatePackage, MRPG_ValueCheckPackage);
