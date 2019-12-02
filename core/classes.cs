640ab51a874c6f83ff11fd1751eea0f46f9b21d9
ConanODoyle <samuel.w.sze@gmail.com>
2019-12-02 09:04:22 -0800

    Add extra classes, tweak scaling functions

--- core/classes.cs
+++ core/classes.cs
@@ -1,29 +1,49 @@
 $MRPGClass_Swordsman = new ScriptObject(MRPGClasses) {
 	class = "Swordsman";
 	armor = 3;
-	armorPerLevel = "x";
+	armorPerLevel = "1 + 0.25 * x";
 	resist = 3;
-	resistPerLevel = "0.5 * x";
+	resistPerLevel = "0.5 + 0.25 * x";
 	maxDamage = 120;
-	healthPerLevel = "x * 10";
+	healthPerLevel = "x * 5 + 5";
 };
 
 $MRPGClass_Bowman = new ScriptObject(MRPGClasses) {
 	class = "Bowman";
 	armor = 1;
-	armorPerLevel = "0.5 * x";
+	armorPerLevel = "0.5";
 	resist = 1;
-	resistPerLevel = "0.5 * x";
+	resistPerLevel = "0.5";
 	maxDamage = 100;
-	healthPerLevel = "x * 10";
+	healthPerLevel = "5 + x";
 };
 
 $MRPGClass_Gunman = new ScriptObject(MRPGClasses) {
 	class = "Gunman";
 	armor = 1;
-	armorPerLevel = "x";
+	armorPerLevel = "0.75";
 	resist = 1;
-	resistPerLevel = "x";
+	resistPerLevel = "0.75";
 	maxDamage = 80;
-	healthPerLevel = "x * 5";
+	healthPerLevel = "5";
+};
+
+$MRPGClass_Mage = new ScriptObject(MRPGClasses) {
+	class = "Mage";
+	armor = 0;
+	armorPerLevel = "0.5";
+	resist = 2;
+	resistPerLevel = "1";
+	maxDamage = 80;
+	healthPerLevel = "5";
+};
+
+$MRPGClass_Healer = new ScriptObject(MRPGClasses) {
+	class = "Healer";
+	armor = 1;
+	armorPerLevel = "0.5";
+	resist = 1;
+	resistPerLevel = "0.5";
+	maxDamage = 80;
+	healthPerLevel = "10";
 };
