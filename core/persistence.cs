95987ca3c1813709b4fc863501e705b682d1e8cc
ConanODoyle <samuel.w.sze@gmail.com>
2019-12-02 16:53:04 -0800

    Remove persistence data for level/exp/gold

--- core/persistence.cs
+++ core/persistence.cs
@@ -3,10 +3,10 @@ if (isFile("Add-ons/Script_Player_Persistence/server.cs"))
 	exec("Add-ons/Script_Player_Persistence/server.cs");
 }
 
-RegisterPersistenceVar("level", false, "");
-RegisterPersistenceVar("exp", false, "");
+// RegisterPersistenceVar("level", false, "");
+// RegisterPersistenceVar("exp", false, "");
 
-RegisterPersistenceVar("maxDamage", false, "");
-RegisterPersistenceVar("armor", false, "");
-RegisterPersistenceVar("resist", false, "");
-RegisterPersistenceVar("class", false, "");
+// RegisterPersistenceVar("maxDamage", false, "");
+// RegisterPersistenceVar("armor", false, "");
+// RegisterPersistenceVar("resist", false, "");
+// RegisterPersistenceVar("class", false, "");
